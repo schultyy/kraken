e04c728e03f0c1b3847ef4ced9ffb95173ad68c7
Jan Schulte <hello@unexpected-co.de>
2013-11-27 22:22:26 +0100

    Added SubscriptionWindowController

---
+++ kraken/SubscriptionWindowController.h
@@ -0,0 +1,13 @@
+//
+//  SubscriptionWindowController.h
+//  kraken
+//
+//  Created by Jan Schulte on 27/11/13.
+//  Copyright (c) 2013 schultyy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SubscriptionWindowController : NSWindowController
+
+@end
