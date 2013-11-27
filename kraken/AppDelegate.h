9430958cfac6b73ab68753bb3018651a8b5787a0
Jan Schulte <hello@unexpected-co.de>
2013-11-27 22:18:48 +0100

    Initial Commit

---
+++ kraken/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  kraken
+//
+//  Created by Jan Schulte on 27/11/13.
+//  Copyright (c) 2013 schultyy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ kraken/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  kraken
+//
+//  Created by Jan Schulte on 27/11/13.
+//  Copyright (c) 2013 schultyy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
